435488ebddef363c2f09a8d6af8fef798c3ef53e
sean <kihongesean@gmail.com>
2025-07-14 22:11:30 +0300

    i

---
+++ lesson 3/file.py
@@ -0,0 +1,12 @@
+number = int(input("Enter number to check : "))
+print("Number to be checked:",number)
+
+if number%2==0 :
+    if number > 50:
+       print("This is a bigger number ") 
+    else:
+        print("This is a smaller number ")
+    print("This is an Even number")  
+
+else:
+ print("this is an odd number")
---
+++ python/lesson2/operators.py
@@ -0,0 +1,18 @@
+name="Sean"
+age=18
+isstudent=True
+weight=70
+
+print('Name:', name)
+print('Data Type of Name is', type(name))
+print('Age:', age)
+print('Data Type of Age is', type(age<))
+print('weight:',weight)
+print('Data Type of Weight is', type(weight))
+print('\n After Type Casting....')
+age=str(age)
+print('age')
+print('Data Type of age is', type(age))
+weight=int(weight)
+print('weight')
+print('Data Type of Weight is', type(weight))
